52982c567175c5952d6cb889af5bd76511aa4c04
François-Denis Gonthier <fdgonthier@lostwebsite.net>
2014-01-27 12:02:39 -0500

    CPU % calculation is back and probably closer to the truth than it was before.

--- public/javascripts/views.js
+++ public/javascripts/views.js
@@ -1,4 +1,13 @@
 var ChartView = Backbone.View.extend({
+
+    _colorIdx: 0,
+    _colors: [
+        "#ff0000", "#00ffff", "#0000ff", "#0000a0", "#add8e6",
+        "#800080", "#ffff00", "#00ff00", "#ff00ff", "#ffffff",
+        "#c0c0c0", "#808080", "#000000", "#ffa500", "#a52a2a",
+        "#800000", "#008000", "#808000"
+    ],
+
     render: function () {
         this.$el.append($("<canvas id='graphCanvas'></canvas>")
             .attr("height", 50)
@@ -24,7 +33,7 @@ var ChartView = Backbone.View.extend({
         if (!self._series[ci.get("no")]) {
             self._series[ci.get("no")] = new TimeSeries();
             self._smoothie.addTimeSeries(self._series[ci.get("no")],
-                { lineWidth: 2, strokeStyle:'#00ff00'});
+                { lineWidth: 2, strokeStyle: this._colors[this._colorIdx++] });
 
             cpuInfo.on("change:userPct", function (m) {
                 self._series[m.get("no")].append(new Date().getTime(), m.get("userPct"));
