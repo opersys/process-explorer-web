b1ba36222d03b14cc85bba677805552c39f6ed08
François-Denis Gonthier <francois-denis.gonthier@opersys.com>
2014-01-31 11:55:51 -0500

    The grid is now inside a Backbone.js view.

--- public/javascripts/pstree.js
+++ public/javascripts/pstree.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var dataView, grid,
+var procView,
     // ProcessCollection object, collection of all process.
     ps = new ProcessCollection(),
     // Global CPU Info
@@ -14,74 +14,10 @@ var dataView, grid,
     // Mem % chart
     memChart;
 
-var pidFormatter = function (row, cell, value, columnDef, proc) {
-    if (!ps.treeView)
-        return grid.getOptions().defaultFormatter(row, cell, value, columnDef, proc);
-
-    var spacer = "<span style='display: inline-block; height: 1px; width: "
-        + (15 * proc.get("ui-indent")) + "px'></span>";
-    var val = proc.get("pid") + " [" + proc.get("name") + "]";
-
-    if (_.size(proc.get("ui-children")) > 0) {
-        if (proc.get("ui-collapsed"))
-            return spacer + " <span class='toggle expand'></span>&nbsp;" + val;
-        else
-            return spacer + " <span class='toggle collapse'></span>&nbsp;" + val;
-    } else
-        return spacer + " <span class='toggle'></span>&nbsp;" + val;
-};
-
-var memoryFormatter = function (row, cell, value, columnDef, proc) {
-    return Humanize.filesizeformat(proc.get(columnDef.field));
-};
-
-var percentFormatter = function (row, cell, value, columnDef, proc) {
-    return proc.get(columnDef.field).toFixed(1) + "%";
-};
-
-var timeFormatter = function (row, cell, value, columnDef, proc) {
-    var v = proc.get(columnDef.field);
-
-    if (!v || v == 0) return "00:00";
-
-    var m = moment.utc(v, "X");
-    if (m.hours() == 0)
-        return m.format("mm:ss");
-    else
-        return m.format("hh[h]mm");
-}
-
-// Update a single process item.
-function updateProcess(fname, proc, v, opts) {
-    var colIdx, rowIdx;
-
-    colIdx = grid.getColumnIndex(fname);
-    rowIdx = proc.get("ui-row");
-
-    grid.flashCell(rowIdx, colIdx, 750);
-}
-
 var resizeWindow = function () {
-    var doResize = false;
-
-    // Change the size properties of the grid so that it fits inside the layout.
-    var jqGrid = $("#grid");
-    var jqCont = $("#grid-container");
-
-    if (jqGrid.width() != jqCont.width()) {
-        jqGrid.width(jqCont.width());
-        doResize = true;
-    }
-
-    if (jqGrid.height() != jqCont.height()) {
-        jqGrid.height(jqCont.height());
-        doResize = true;
-    }
-
-    if (doResize) {
-        grid.resizeCanvas();
-        grid.autosizeColumns();
-    }
+    procView.size(
+        $(window).width(),
+        $(window).height() - $("#graphContainer").height() - 1);
 };
 
 var globalProcessUpdate = function () {
@@ -127,83 +63,10 @@ var globalProcessUpdate = function () {
                 });
             }
         });
-
-        resizeWindow();
-    });
-};
-
-var initGrid = function () {
-    var columns = [
-        { id: "pid", name: "PID", field: "id",
-            formatter: pidFormatter  },
-        { id: "state", name: "S", field: "state", minWidth: 15, maxWidth: 20 },
-        { id: "prio", name: "PRI", field: "prio", minWidth: 20, maxWidth: 30 },
-        { id: "cpuPct", name: "%CPU", field: "cpuPct", minWidth: 40, maxWidth: 60,
-            formatter: percentFormatter, sortable: true },
-        { id: "memPct", name: "%Mem", field: "memPct", minWidth: 40, maxWidth: 60,
-            formatter: percentFormatter, sortable: true },
-        { id: "vss", name: "VSS", field: "vss", minWidth: 60, maxWidth: 80,
-            formatter: memoryFormatter, sortable: true },
-        { id: "rss", name: "RSS", field: "rss", minWidth: 60, maxWidth: 80,
-            formatter: memoryFormatter, sortable: true },
-        { id: "shm", name: "SHM", field: "shm", minwidth: 60, maxWidth: 80,
-            formatter: memoryFormatter, sortable: true },
-        { id: "time", name: "Time", field: "time", minWidth: 60, maxWidth: 80,
-            formatter: timeFormatter, sortable: true },
-        { id: "cmdline", name: "Command line", field: "cmdline", minWidth: 100 }
-    ];
-
-    var options = {
-        enableColumnReorder: false,
-        formatterFactory: Slickback.BackboneModelFormatterFactory,
-        forceFitColumns: true
-    };
-
-    grid = new Slick.Grid("#grid", ps, columns, options);
-
-    grid.onClick.subscribe(function (e, args) {
-        var proc = grid.getDataItem(args.row);
-
-        if ($(e.target).hasClass("toggle")) {
-            if (!proc.get("ui-collapsed"))
-                proc.set({"ui-collapsed": true});
-            else
-                proc.set({"ui-collapsed": false});
-        }
-
-        e.stopImmediatePropagation();
-    });
-
-    ps.on("change:cpuPct", function (m, v, opts) { updateProcess("cpuPct", m, v, opts); });
-    ps.on("change:memPct", function (m, v, opts) { updateProcess("memPct", m, v, opts); });
-    ps.on("change:vss", function (m, v, opts) { updateProcess("vss", m, v, opts) });
-    ps.on("change:rss", function (m, v, opts) { updateProcess("rss", m, v, opts) });
-
-    ps.onRowCountChanged.subscribe(function () {
-        grid.updateRowCount();
-        grid.render();
-    });
-
-    ps.onRowsChanged.subscribe(function () {
-        grid.invalidate();
-        grid.render();
-    });
-
-    grid.onSort.subscribe(function (e, args) {
-        ps.sortPs(args.sortCol.field, args.sortAsc);
-
-        grid.invalidate();
-        grid.render();
-    });
-
-    ps.on("remove", function (proc) {
-        console.log("Process " + proc.get("name") + "[" + proc.get("pid") + "] removed.");
     });
 };
 
 $(document).ready(function () {
-    initGrid();
-
     cpuChart = new ChartView({
         el: $("#cpuChart"),
         max: 100,
@@ -214,6 +77,14 @@ $(document).ready(function () {
         el: $("#memChart"),
         min: 0,
         delay: 5000
+    });
+    procView = new ProcessView({
+        el: $("#grid"),
+        ps: ps
+    });
+
+    ps.on("remove", function (proc) {
+        console.log("Process " + proc.get("name") + "[" + proc.get("pid") + "] removed.");
     });
 
     $(window).resize(resizeWindow);
--- public/javascripts/views.js
+++ public/javascripts/views.js
@@ -1,3 +1,188 @@
+var ProcessView = Backbone.View.extend({
+
+    _pidFormatter: function (row, cell, value, columnDef, proc) {
+        if (!ps.treeView)
+            return this._grid.getOptions().defaultFormatter(row, cell, value, columnDef, proc);
+
+        var spacer = "<span style='display: inline-block; height: 1px; width: "
+            + (15 * proc.get("ui-indent")) + "px'></span>";
+        var val = proc.get("pid") + " [" + proc.get("name") + "]";
+
+        if (_.size(proc.get("ui-children")) > 0) {
+            if (proc.get("ui-collapsed"))
+                return spacer + " <span class='toggle expand'></span>&nbsp;" + val;
+            else
+                return spacer + " <span class='toggle collapse'></span>&nbsp;" + val;
+        } else
+            return spacer + " <span class='toggle'></span>&nbsp;" + val;
+    },
+
+    _memoryFormatter: function (row, cell, value, columnDef, proc) {
+        return Humanize.filesizeformat(proc.get(columnDef.field));
+    },
+
+    _percentFormatter: function (row, cell, value, columnDef, proc) {
+        return proc.get(columnDef.field).toFixed(1) + "%";
+    },
+
+    _timeFormatter: function (row, cell, value, columnDef, proc) {
+        var v = proc.get(columnDef.field);
+
+        if (!v || v == 0) return "00:00";
+
+        var m = moment.utc(v, "X");
+        if (m.hours() == 0)
+            return m.format("mm:ss");
+        else
+            return m.format("hh[h]mm");
+    },
+
+    _columns: [
+        { id: "pid", name: "PID", field: "id" },
+        { id: "state", name: "S", field: "state", minWidth: 15, maxWidth: 20 },
+        { id: "prio", name: "PRI", field: "prio", minWidth: 20, maxWidth: 30 },
+        { id: "cpuPct", name: "%CPU", field: "cpuPct", minWidth: 40, maxWidth: 60, sortable: true },
+        { id: "memPct", name: "%Mem", field: "memPct", minWidth: 40, maxWidth: 60, sortable: true },
+        { id: "vss", name: "VSS", field: "vss", minWidth: 60, maxWidth: 80, sortable: true },
+        { id: "rss", name: "RSS", field: "rss", minWidth: 60, maxWidth: 80, sortable: true },
+        { id: "shm", name: "SHM", field: "shm", minwidth: 60, maxWidth: 80, sortable: true },
+        { id: "time", name: "Time", field: "time", minWidth: 60, maxWidth: 80, sortable: true },
+        { id: "cmdline", name: "Command line", field: "cmdline", minWidth: 100 }
+    ],
+
+    _options: {
+        enableColumnReorder: false,
+        formatterFactory: Slickback.BackboneModelFormatterFactory,
+    },
+
+    _updateProcess: function (fname, proc, v, opts) {
+        var colIdx, rowIdx;
+
+        colIdx = this._grid.getColumnIndex(fname);
+        rowIdx = proc.get("ui-row");
+
+        this._grid.flashCell(rowIdx, colIdx, 750);
+    },
+
+    _onGridClick: function (e, args) {
+        var proc = this._grid.getDataItem(args.row);
+
+        if ($(e.target).hasClass("toggle")) {
+            if (!proc.get("ui-collapsed"))
+                proc.set({"ui-collapsed": true});
+            else
+                proc.set({"ui-collapsed": false});
+        }
+
+        e.stopImmediatePropagation();
+    },
+
+    _onGridSort: function (e, args) {
+        this._ps.sortPs(args.sortCol.field, args.sortAsc);
+        this._grid.invalidate();
+        this._grid.render();
+    },
+
+    _onPsRowCountChanged: function () {
+        this._grid.updateRowCount();
+        this._grid.render();
+    },
+
+    _onPsRowsChanged: function () {
+        this._grid.invalidate();
+        this._grid.render();
+    },
+
+    size: function (x, y) {
+        if (this.$el.width() == x && this.$el.height() == y)
+            return;
+
+        this.$el
+            .width(x)
+            .height(y);
+
+        this._grid.resizeCanvas();
+        this._grid.autosizeColumns();
+    },
+
+    initialize: function (opts) {
+        var self = this;
+
+        // Create and initialize the grid as per:
+        // https://github.com/mleibman/SlickGrid/blob/gh-pages/examples/example-explicit-initialization.html
+        this._grid = new Slick.Grid(this.$el, ps, this._columns, this._options);
+
+        // Initialize the column formatters and call the formatters
+        // in the context of the view.
+
+        this._columns[this._grid.getColumnIndex("pid")].formatter = function () {
+            return self._pidFormatter.apply(self, arguments);
+        };
+
+        this._columns[this._grid.getColumnIndex("cpuPct")].formatter = function () {
+            return self._percentFormatter.apply(self, arguments);
+        };
+
+        this._columns[this._grid.getColumnIndex("memPct")].formatter = function () {
+            return self._percentFormatter.apply(self, arguments);
+        };
+
+        this._columns[this._grid.getColumnIndex("rss")].formatter = function () {
+            return self._memoryFormatter.apply(self, arguments);
+        };
+
+        this._columns[this._grid.getColumnIndex("vss")].formatter = function () {
+            return self._memoryFormatter.apply(self, arguments);
+        };
+
+        this._columns[this._grid.getColumnIndex("shm")].formatter = function () {
+            return self._memoryFormatter.apply(self, arguments);
+        };
+
+        this._columns[this._grid.getColumnIndex("time")].formatter = function () {
+            return self._timeFormatter.apply(self, arguments);
+        };
+
+        this._grid.onClick.subscribe(function (e, args) {
+            self._onGridClick.apply(self, [e, args]);
+        });
+        this._grid.onSort.subscribe(function (e, args) {
+            self._onGridSort.apply(self, [e, args]);
+        });
+
+        this._ps = opts.ps;
+
+        //this._cont.on("resize", this._onResize());
+
+        this._ps.on("change:cpuPct", function (m, v, opts) {
+            self._updateProcess("cpuPct", m, v, opts);
+        });
+        this._ps.on("change:memPct", function (m, v, opts) {
+            self._updateProcess("memPct", m, v, opts);
+        });
+        this._ps.on("change:vss", function (m, v, opts) {
+            self._updateProcess("vss", m, v, opts)
+        });
+        this._ps.on("change:rss", function (m, v, opts) { 
+            self._updateProcess("rss", m, v, opts)
+        });
+
+        // FIXME: SlickBack style events, we don't really need that.
+        this._ps.onRowCountChanged.subscribe(function () {
+            self._onPsRowCountChanged.apply(self);
+        });
+        this._ps.onRowsChanged.subscribe(function () {
+            self._onPsRowsChanged.apply(self)
+        });
+
+        this.render();
+    },
+
+    render: function () {
+        this._grid.resizeCanvas();
+    }
+});
+
 var ChartView = Backbone.View.extend({
 
     _colorIdx: 0,
