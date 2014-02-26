726c0db360ec2928011e7b754f22a1a63f8d8689
François-Denis Gonthier <francois-denis.gonthier@opersys.com>
2014-02-26 15:07:04 -0500

    Massive rework of the app that is now exclusively targetting Android. Added a Gruntfile to prepare the app in the 'dist' directory. All Javascript files are packed.

---
+++ Gruntfile.js
@@ -0,0 +1,146 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+
+        css_crunch: {
+            dist: {
+                src: "assets/css/",
+                dest: "dist/public/css/<%= pkg.name %>_styles.css",
+                minify: true,
+                optimize: true,
+                copy: true
+            }
+        },
+
+        mkdir: {
+            dist: {
+                options: {
+                    create: [
+                        "dist/public/css",
+                        "dist/public/js",
+                        "dist/routes",
+                        "dist/views"
+                    ]
+                }
+            }
+        },
+
+        copyto: {
+            dist: {
+                files: [
+                    { cwd: "./bin/arm", dest: "dist/", src:
+                        [
+                            "node",
+                            "pswalk.node"
+                        ]
+                    },
+                    { cwd: ".", dest: "dist/", src:
+                        [
+                            "app.js",
+                            "package.json"
+                        ]
+                    },
+                    { cwd: "./", dest: "dist/", src:
+                        [
+                            "public/**/*",
+                            "routes/*.js",
+                            "app.js"
+                        ]
+                    },
+                    { cwd: "./assets/css", dest: "dist/public/css/", src:
+                        [
+                            "font-awesome.min.css"
+                        ]
+                    }
+                ]
+            }
+        },
+
+        concat: {
+            options: { separator: ";" },
+            dist_css: {
+                // Font Awesome is not included since it seems it has to be
+                // loaded alone for the web font to be properly loaded in Chrome.
+                src: [
+                    "assets/css/style.css",
+                    "assets/css/slick.grid.css",
+                    "assets/css/slick-default-theme.css",
+                    "assets/css/w2ui-1.3.1.min.css",
+                ],
+                dest: "dist/public/css/<%= pkg.name %>_styles.css"
+            },
+            dist_libs: {
+                // Source files. Order matters.
+                src: [
+                    "assets/jslib/jquery-2.0.3.min.js",
+                    "assets/jslib/jquery.event.drag-2.2.js",
+                    "assets/jslib/jquery.ba-throttle-debounce.min.js",
+                    "assets/jslib/underscore-min.js",
+                    "assets/jslib/backbone.js",
+                    "assets/jslib/humanize.min.js",
+                    "assets/jslib/Queue.js",
+                    "assets/jslib/moment.min.js",
+                    "assets/jslib/smoothie.js",
+                    "assets/jslib/w2ui-1.3.1.min.js",
+                    "assets/jslib/slickgrid/slick.core.js",
+                    "assets/jslib/slickgrid/slick.grid.js",
+                    "assets/jslib/slickgrid/slick.formatters.js",
+                    "assets/jslib/slickgrid/plugins/slick.rowselectionmodel.js",
+                    "assets/jslib/slickback.min.js",
+                ],
+                dest: "dist/public/js/<%= pkg.name %>_libs.js"
+            },
+            dist_main: {
+                src: [ "assets/js/*.js" ],
+                dest: "dist/public/js/<%= pkg.name %>_main.js"
+            }
+        },
+
+        uglify: {
+            dist: {
+                files: {
+                    "dist/js/public/<%= pkg.name %>_libs.min.js": ["<%= concat.dist_libs.dest %>"],
+                    "dist/js/public/<%= pkg.name %>_main.min.js": ["<%= concat.dist_main.dest %>"]
+                }
+            }
+        },
+
+        cssmin: {
+            dist: {
+                src: "<%= concat.dist_css.dest %>",
+                dest: "<%= concat.dist_css.dest %>"
+            }
+        },
+
+        exec: {
+            npm_install: {
+                command: "npm --production install",
+                stdout: false,
+                stderr: false,
+                cwd: "dist"
+            }
+        },
+
+        jade: {
+            html: {
+                src: ["views/*.jade"],
+                dest: "dist/public/",
+                options: {
+                    client: false
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-mkdir");
+    grunt.loadNpmTasks("grunt-css");
+    grunt.loadNpmTasks("grunt-copy-to");
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-jade");
+    grunt.loadNpmTasks("grunt-exec");
+
+    grunt.registerTask("default", ["mkdir", "copyto", "concat", "uglify", "cssmin", "jade", "exec"]);
+}
+
---
+++ assets/js/pstree.js
@@ -0,0 +1,188 @@
+var procView, logCatView,
+    // ProcessCollection object, collection of all process.
+    ps = new ProcessCollection(),
+    // Global CPU Info
+    globalCpu = new CpuInfo(),
+    // Individual CPU info
+    cpuInfo = new CpuInfoCollection(),
+    // Logcat
+    logCatLines = new LogCatLines(),
+    // Memory info
+    memInfo = new MemInfo(),
+    // Concept
+    logcat = new LogCat(),
+    // CPU % chart
+    cpuChart,
+    // Mem % chart
+    memChart;
+
+var resizeWindow = function () {
+    $("#mainLayout")
+        .width($(window).width())
+        .height($(window).height());
+    w2ui["mainLayout"].resize();
+};
+
+function uncompress(clist) {
+    var ctab = clist.ctab;
+    var lstlst = clist.list;
+    var r = [];
+
+    _.each(lstlst, function (lst) {
+        var obj = {};
+
+        _.each(ctab, function (k) {
+            obj[k] = lst.shift();
+        });
+        r.push(obj);
+    });
+    return r;
+}
+
+var globalProcessUpdate = function () {
+    $.ajax("/sysinfo").done(function (sysinfo) {
+        var totalDeltaTime;
+
+        console.log("Process list update.");
+
+        globalCpu.set(sysinfo.cpuinfo.global);
+        cpuInfo.set(sysinfo.cpuinfo.cpus);
+        memInfo.set(sysinfo.meminfo);
+
+        ps.set(uncompress(sysinfo.ps));
+
+        // Update the CPU graph
+        cpuInfo.each(function (ci) {
+            cpuChart.serie(ci.get("no"), "userPct", ci);
+        });
+
+        memChart.serie("memUsed", "memUsed", memInfo);
+        memChart.serie("memShared", "memShared", memInfo);
+
+        ps.each(function (proc) {
+            proc.updateCpuPct(globalCpu.get("totalDeltaTime") / globalCpu.get("ncpu"));
+            proc.updateMemPct(memInfo.get("memTotal"));
+        });
+
+        // Calculate the process tree
+        ps.each(function (e) {
+            if (e.get("pid") != 0 && e.get("ppid") != undefined) {
+                var ppsItem = ps.get(e.get("ppid"));
+                var ppsItemCh = ppsItem.get("ui-children");
+
+                // Add the new children to the parent.
+                ppsItemCh[e.get("pid")] = e;
+                ppsItem.set({"ui-children": ppsItemCh});
+
+                // Set the indent and the order of the current process in the
+                // tree view.
+                e.set({
+                    "ui-indent": ppsItem.get("ui-indent") +  1,
+                    "ui-parent": ppsItem
+                });
+            }
+        });
+    });
+};
+
+$(document).ready(function () {
+    var pidFilterMode = false;
+
+    $('#mainLayout').w2layout({
+        name: 'mainLayout',
+        padding: 4,
+        panels: [
+            {
+                type: "top",
+                size: 105,
+                content: "<div id='cpuGraph'></div><div id='memGraph'></div>"
+            },
+            {
+                type: "main",
+                toolbar: {
+                    items: [
+                        { type: "check", id: "btnPlay", caption: "Play", icon: "icon-play" }
+                    ]
+                }
+            },
+            {
+                type: "preview",
+                size: 200,
+                resizer: 5,
+                resizable: true,
+                toolbar: {
+                    name: "tbPreview",
+                    items: [
+                        { type: "check",  id: "btnFilterByProcess", caption: "Filter", icon: "icon-long-arrow-down" },
+                        { type: "button", id: "btnClear",           caption: "Clear",  icon: "icon-remove" }
+                    ],
+                    onClick: function (ev) {
+                        if (ev.target == "btnClear")
+                            logCatLines.reset();
+
+                        if (ev.target == "btnFilterByProcess") {
+                            pidFilterMode = !pidFilterMode;
+
+                            if (!pidFilterMode)
+                                logCatLines.clearPid();
+                        }
+                    }
+                }
+            }
+        ],
+        onResize: function (ev) {
+            console.log("onResize");
+
+            if (procView)
+                procView.autoResize();
+            if (logCatView)
+                logCatView.autoResize();
+        }
+    });
+
+    $(w2ui["mainLayout"].el("top")).append($("#graphLayout"));
+
+    cpuChart = new ChartView({
+        el: $("#cpuGraph"),
+        max: 100,
+        min: 0,
+        delay: 5000,
+        width: 300,
+        height: 101
+    });
+    memChart = new ChartView({
+        el: $("#memGraph"),
+        min: 0,
+        delay: 5000,
+        width: 300,
+        height: 101
+    });
+    procView = new ProcessView({
+        el: $(w2ui["mainLayout"].el("main")),
+        ps: ps
+    });
+    logCatView = new LogCatView({
+        el: $(w2ui["mainLayout"].el("preview")),
+        logcat: logCatLines
+    });
+
+    ps.on("remove", function (proc) {
+        console.log("Process " + proc.get("name") + "[" + proc.get("pid") + "] removed.");
+    });
+
+    procView.on("onProcessSelected", function (el) {
+        if (pidFilterMode)
+            logCatLines.setPid(el.get("pid"));
+    });
+
+    $(window).resize($.debounce( 100, resizeWindow));
+
+    // Update the process list right now.
+    globalProcessUpdate();
+
+    // The process collection updates every 5 seconds.
+    window.setInterval(globalProcessUpdate, 5000);
+
+    // Reformat the window content.
+    resizeWindow();
+});
