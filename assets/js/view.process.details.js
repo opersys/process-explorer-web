4ba3b3549780719cdde96008cfa989ab65c217ec
benjamin vanheuverzwijn <benjamin.vanheuverzwijn@opersys.com>
2014-12-04 12:13:38 -0500

    Added the frontend to display process details environment variables

--- assets/js/view.process.details.js
+++ assets/js/view.process.details.js
@@ -16,11 +16,47 @@
 
 var ProcessDetailsView = Backbone.View.extend({
 
+    getEnvironment: function(data) {
+        var table = document.createElement('table');
+        var table_header =document.createElement('thead');
+        var table_body = document.createElement('tbody');
+
+        // Table header
+        var header = document.createElement('tr');
+        var header_name = document.createElement('th');
+        var header_value = document.createElement('th');
+        header_name.appendChild(document.createTextNode('Name'));
+        header_value.appendChild(document.createTextNode('Value'));
+        header.appendChild(header_name);
+        header.appendChild(header_value);
+        table_header.appendChild(header);
+
+        // Table body
+        for (var name in data.variables) {
+            if (data.variables.hasOwnProperty(name)) {
+                var row = document.createElement('tr');
+                var cell_name = document.createElement('td');
+                var cell_value = document.createElement('td');
+                cell_name.appendChild(document.createTextNode(name));
+                row.appendChild(cell_name);
+
+                cell_value.appendChild(document.createTextNode(data.variables[name]));
+                row.appendChild(cell_value);
+
+                table_body.appendChild(row);
+            }
+        }
+
+        table.appendChild(table_header);
+        table.appendChild(table_body);
+
+        return table;
+    },
+
     initialize: function (opts) {
         var self = this;
 
-        this._ps = opts.ps;
-        this._options = opts.options;
+        this._process = null;
 
         var pstyle = 'background-color: #F5F6F7; border: 1px solid #dfdfdf; padding: 5px;';
 
@@ -29,8 +65,8 @@ var ProcessDetailsView = Backbone.View.extend({
             name: 'processdetails_layout',
             panels: [
                 { type: 'main',
-                    content: '',
                     title: '<div id="processdetails_title"></div>',
+                    content: '<div id="processdetails_content"></div>',
                     tabs: {
                         name: 'processDetailsTabs',
                         active: 'files',
@@ -41,7 +77,16 @@ var ProcessDetailsView = Backbone.View.extend({
                             { id: 'network', caption: 'Network', closable: false },
                         ],
                         onClick: function (event) {
-                            $('#tab-content').html('Tab: ' + event.target);
+                            if (event.target == "environment") {
+                                $.ajax({
+                                    url: "/process/environ?pid=" + self._process.get("pid"),
+                                }).done(function(data) {
+                                    $('#processdetails_content').html(self.getEnvironment(data));
+                                });
+                            }
+                            else {
+                                $('#processdetails_content').html('Tab: ' + event.target);
+                            }
                         }
                     }
                 },
@@ -65,7 +110,7 @@ var ProcessDetailsView = Backbone.View.extend({
 
         if ($(self.$el).hasClass("collapsed")) {
             w2ui["processdetails_collapse"].get("collapse").icon = "icon-chevron-right";
-            w2ui["ps_layout"].sizeTo("right", 250);
+            w2ui["ps_layout"].sizeTo("right", $(window).width() * 0.35);
         }
         else {
             w2ui["processdetails_collapse"].get("collapse").icon = "icon-chevron-left";
@@ -76,5 +121,9 @@ var ProcessDetailsView = Backbone.View.extend({
         $(self.$el).toggleClass("collapsed");
 
         return;
-    }
+    },
+
+    setProcess: function(process) {
+        this._process = process;
+    },
 });
